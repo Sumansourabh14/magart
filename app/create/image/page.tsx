383c4f7a7ca05194acb96dd58544ff031640bdb2
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-13 18:52:24 +0530

    added heading 1 on templates and upload image pages

--- app/create/image/page.tsx
+++ app/create/image/page.tsx
@@ -1,11 +1,15 @@
 import UploadImage from "@/components/sections/UploadImage";
+import H1 from "@/components/text/H1";
 import React from "react";
 
 const Create = () => {
   return (
-    <div>
-      <UploadImage />
-    </div>
+    <section>
+      <H1 title="Create Image" />
+      <section className="py-10">
+        <UploadImage />
+      </section>
+    </section>
   );
 };
 
--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,13 +1,15 @@
 import DisplayTemplate from "@/components/sections/DisplayTemplate";
+import H1 from "@/components/text/H1";
 
 export default function Templates() {
   return (
-    <div className="min-h-screen py-8 px-8 font-[family-name:var(--font-geist-sans)]">
-      <main className="py-20">
+    <div className="px-8 font-[family-name:var(--font-geist-sans)]">
+      <H1 title="Templates" />
+      <section className="py-10">
         <section className="max-w-[1400px] mx-auto">
           <DisplayTemplate />
         </section>
-      </main>
+      </section>
     </div>
   );
 }
